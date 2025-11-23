8139a52576dd671ba0a2d6e2632855e7a22f19d9
01-HanVietAnh-0090566-66TH1 <hanvietanh127@gmail.com>
2025-11-23 16:30:45 +0700

    Update

---
+++ Đồ án Lập trình/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using TinhtoancotBTCT.View;
+
+namespace TinhtoancotBTCT
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Khởi động bằng Login.xaml
+            var loginWindow = new View.Login();
+            loginWindow.Show();
+        }
+    }
+}
+
