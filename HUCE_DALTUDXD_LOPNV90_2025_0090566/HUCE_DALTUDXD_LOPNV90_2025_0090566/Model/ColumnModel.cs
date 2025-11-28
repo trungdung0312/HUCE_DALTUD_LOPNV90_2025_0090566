552a607b751e8995fc2dc2d287c9c5f3ff345a36
namcoto83-droid <namcoto83@gmail.com>
2025-11-28 22:44:10 +0700

    updateModel P2

---
+++ HUCE_DALTUDXD_LOPNV90_2025_0090566/HUCE_DALTUDXD_LOPNV90_2025_0090566/Model/ColumnModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HUCE_DALTUDXD_LOPNV90_2025_0090566.Model
+{
+    public class Column
+    {
+        public double Width { get; set; }      // b (mm)
+        public double Height { get; set; }     // h (mm)
+        public double AxialForce { get; set; } // N (kN)
+        public double MomentX { get; set; }    // Mx (kNm)
+        public double MomentY { get; set; }    // My (kNm)
+
+        public Rebar Rebar { get; set; }
+
+        public Column(double width, double height, Rebar rebar)
+        {
+            Width = width;
+            Height = height;
+            Rebar = rebar;
+        }
+    }
+}
